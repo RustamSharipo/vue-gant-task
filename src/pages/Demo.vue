9e7ab19cd62ba63f46ff5c541974aaf5f8379619
qwerty <qwerty1>
2022-11-15 16:08:00 +0500

    task

--- src/pages/Demo.vue
+++ src/pages/Demo.vue
@@ -1,214 +1,255 @@
 <template>
-  <gantt-elastic :tasks="tasks" :options="options">
-  <gantt-elastic-header slot="header"></gantt-elastic-header>
-  <gantt-elastic-footer slot="footer"></gantt-elastic-footer>
-</gantt-elastic>
+  <header>hello gantt</header>
+
+  <div>
+    <GanttElastic :options="options" :tasks="tasks">
+      <template v-slot:header>
+        <GanttElasticHeader2></GanttElasticHeader2>
+      </template>
+    </GanttElastic>
+  </div>
 </template>
-
 <script>
-
-
-import GanttElastic from "gantt-elastic";
-// import GanttHeader from "gantt-elastic-header";
 import dayjs from "dayjs";
+import { GanttElastic, GanttElasticHeader2 } from "gantt-elastic-wrapvue3";
+import "gantt-elastic-wrapvue3";
 
-
-function getDate(hours) {
-        const currentDate = new Date();
-        const currentYear = currentDate.getFullYear();
-        const currentMonth = currentDate.getMonth();
-        const currentDay = currentDate.getDate();
-        const timeStamp = new Date(currentYear, currentMonth, currentDay, 0, 0, 0).getTime();
-        return new Date(timeStamp + hours * 60 * 60 * 1000).getTime();
-}
-
-const tasks = [
-    {
-      id: 1,
-      label: 'Make some noise',
-      user:
-        '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
-      start: getDate(-24 * 5),
-      duration: 15 * 24 * 60 * 60 * 1000,
-      progress: 85,
-      type: 'project',
-      //collapsed: true,
-    },
-    {
-      id: 2,
-      label: 'With great power comes great responsibility',
-      user:
-        '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
-      parentId: 1,
-      start: getDate(-24 * 4),
-      duration: 4 * 24 * 60 * 60 * 1000,
-      progress: 50,
-      type: 'milestone',
-      collapsed: true,
-      style: {
-        base: {
-          fill: '#1EBC61',
-          stroke: '#0EAC51',
+export default {
+  components: {
+    GanttElastic,
+    GanttElasticHeader2,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          label: "Make some noise",
+          user:
+            '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
+          start: this.getDate(-24 * 5),
+          duration: 12 * 60 * 60 * 1000,
+          progress: 45,
+          type: "project",
         },
-        /*'tree-row-bar': {
-          fill: '#1EBC61',
-          stroke: '#0EAC51'
+        {
+          id: 2,
+          label: "With great power comes great responsibility",
+          user:
+            '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
+
+          start: this.getDate(-24 * 4),
+          duration: 4 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: "milestone",
+          style: {
+            base: {
+              fill: "#1EBC61",
+              stroke: "#0EAC51",
+            },
+            /*'tree-row-bar': {
+            fill: '#1EBC61',
+            stroke: '#0EAC51'
+          },
+          'tree-row-bar-polygon': {
+            stroke: '#0EAC51'
+          }*/
+          },
         },
-        'tree-row-bar-polygon': {
-          stroke: '#0EAC51'
-        }*/
-      },
-    },
-    {
-      id: 3,
-      label: 'Courage is being scared to death, but saddling up anyway.',
-      user:
-        '<a href="https://www.google.com/search?q=John+Wayne" target="_blank" style="color:#0077c0;">John Wayne</a>',
-      parentId: 2,
-      start: getDate(-24 * 3),
-      duration: 2 * 24 * 60 * 60 * 1000,
-      progress: 100,
-      type: 'task',
-    },
-    {
-      id: 4,
-      label: 'Put that toy AWAY!',
-      user:
-        '<a href="https://www.google.com/search?q=Clark+Kent" target="_blank" style="color:#0077c0;">Clark Kent</a>',
-      start: getDate(-24 * 2),
-      duration: 2 * 24 * 60 * 60 * 1000,
-      progress: 50,
-      type: 'task',
-      dependentOn: [3],
-    },
-  ];
-
-
-let options = {
-        maxRows: 100,
-        maxHeight: 300,
+        {
+          id: 3,
+          label: "testMultiple",
+          user:
+            '<a href="https://www.google.com/search?q=Austin+Powers" target="_blank" style="color:#0077c0;">Austin Powers</a>',
+          type: "multiple",
+          subTask: [
+            {
+              id: "3_1",
+              start: this.getDate(-21 * 3),
+              duration: 2 * 24 * 60 * 60 * 1000,
+              progress: 5,
+              type: "task",
+              style: {},
+            },
+            {
+              id: "3_2",
+              start: this.getDate(-2 * 3),
+              duration: 2 * 10 * 60 * 60 * 1000,
+              progress: 100,
+              type: "task",
+              style: {},
+            },
+          ],
+        },
+        {
+          id: 4,
+          label: "Put that toy AWAY!",
+          user:
+            '<a href="https://www.google.com/search?q=Clark+Kent" target="_blank" style="color:#0077c0;">Clark Kent</a>',
+          start: this.getDate(-24 * 2),
+          duration: 2 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: "task",
+          dependentOn: [3],
+        },
+        {
+          id: 5,
+          label:
+            "One billion, gajillion, fafillion... shabadylu...mil...shabady......uh, Yen.",
+          user:
+            '<a href="https://www.google.com/search?q=Austin+Powers" target="_blank" style="color:#0077c0;">Austin Powers</a>',
+          parentId: 4,
+          start: this.getDate(0),
+          duration: 2 * 24 * 60 * 60 * 1000,
+          progress: 10,
+          type: "milestone",
+          style: {
+            base: {
+              fill: "#0287D0",
+              stroke: "#0077C0",
+            },
+          },
+        },
+        {
+          id: 6,
+          label: "Butch Mario and the Luigi Kid",
+          user:
+            '<a href="https://www.google.com/search?q=Mario+Bros" target="_blank" style="color:#0077c0;">Mario Bros</a>',
+          parentId: 5,
+          start: this.getDate(24),
+          duration: 1 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: "task",
+          style: {
+            base: {
+              fill: "#8E44AD",
+              stroke: "#7E349D",
+            },
+          },
+        },
+        {
+          id: 7,
+          label: "Devon, the old man wanted me, it was his dying request",
+          user:
+            '<a href="https://www.google.com/search?q=Knight+Rider" target="_blank" style="color:#0077c0;">Knight Rider</a>',
+          parentId: 5,
+          start: this.getDate(24 * 2),
+          duration: 4 * 60 * 60 * 1000,
+          progress: 20,
+          type: "task",
+        },
+        {
+          id: 8,
+          label: "Hey, Baby! Anybody ever tell you I have beautiful eyes?",
+          user:
+            '<a href="https://www.google.com/search?q=Johhny+Bravo" target="_blank" style="color:#0077c0;">Johhny Bravo</a>',
+          parentId: 7,
+          dependentOn: [7],
+          start: this.getDate(24 * 3),
+          duration: 1 * 24 * 60 * 60 * 1000,
+          progress: 0,
+          type: "task",
+        },
+        {
+          id: 9,
+          label:
+            "This better be important, woman. You are interrupting my very delicate calculations.",
+          user:
+            '<a href="https://www.google.com/search?q=Dexter\'s+Laboratory" target="_blank" style="color:#0077c0;">Dexter\'s Laboratory</a>',
+          parentId: 8,
+          dependentOn: [8, 7],
+          start: this.getDate(24 * 4),
+          duration: 4 * 60 * 60 * 1000,
+          progress: 20,
+          type: "task",
+          style: {
+            base: {
+              fill: "#8E44AD",
+              stroke: "#7E349D",
+            },
+            /*'tree-row-bar-polygon': {
+            stroke: '#7E349D'
+          },
+          'tree-row-bar': {
+            fill: '#8E44AD',
+            stroke: '#7E349D'
+          }*/
+          },
+        },
+        {
+          id: 10,
+          label: "current task",
+          user:
+            '<a href="https://www.google.com/search?q=Johnattan+Owens" target="_blank" style="color:#0077c0;">Johnattan Owens</a>',
+          start: this.getDate(24 * 5),
+          duration: 24 * 60 * 60 * 1000,
+          progress: 0,
+          type: "task",
+        },
+      ],
+      options: {
         title: {
-          label: 'Your project title as html (link or whatever...)',
+          label: "Your project title as html (link or whatever...)",
           html: false,
         },
-        row: {
-          height: 24,
-        },
-        calendar: {
-          hour: {
-            display: false,
-          },
-        },
-        chart: {
-          progress: {
-            bar: false,
-          },
-          expander: {
-            display: true,
-          },
-        },
         taskList: {
-          expander: {
-            straight: false,
-          },
           columns: [
             {
+              id: 2,
+              label: "Description",
+              value: "label",
+              width: 220,
+              expander: true,
+            },
+            {
               id: 1,
-              label: 'ID',
-              value: 'id',
+              label: "ID",
+              value: "id",
               width: 40,
             },
             {
-              id: 2,
-              label: 'Description',
-              value: 'label',
-              width: 200,
-              expander: true,
-              html: true,
-              events: {
-                click({ data, column }) {
-                  alert('description clicked!\n' + data.label);
-                },
-              },
-            },
-            {
               id: 3,
-              label: 'Assigned to',
-              value: 'user',
+              label: "Assigned to",
+              value: "user",
               width: 130,
               html: true,
             },
             {
               id: 3,
-              label: 'Start',
-              value: (task) => dayjs(task.start).format('YYYY-MM-DD'),
+              label: "Start",
+              value: (task) => dayjs(task.start).format("YYYY-MM-DD"),
               width: 78,
             },
             {
               id: 4,
-              label: 'Type',
-              value: 'type',
+              label: "Type",
+              value: "type",
               width: 68,
             },
             {
               id: 5,
-              label: '%',
-              value: 'progress',
+              label: "%",
+              value: "progress",
               width: 35,
-              style: {
-                'task-list-header-label': {
-                  'text-align': 'center',
-                  width: '100%',
-                },
-                'task-list-item-value-container': {
-                  'text-align': 'center',
-                  width: '100%',
-                },
-              },
             },
           ],
         },
-        /*locale:{
-          name: 'pl', // name String
-          weekdays: 'Poniedziałek_Wtorek_Środa_Czwartek_Piątek_Sobota_Niedziela'.split('_'), // weekdays Array
-          weekdaysShort: 'Pon_Wto_Śro_Czw_Pią_Sob_Nie'.split('_'), // OPTIONAL, short weekdays Array, use first three letters if not provided
-          weekdaysMin: 'Pn_Wt_Śr_Cz_Pt_So_Ni'.split('_'), // OPTIONAL, min weekdays Array, use first two letters if not provided
-          months: 'Styczeń_Luty_Marzec_Kwiecień_Maj_Czerwiec_Lipiec_Sierpień_Wrzesień_Październik_Listopad_Grudzień'.split('_'), // months Array
-          monthsShort: 'Sty_Lut_Mar_Kwi_Maj_Cze_Lip_Sie_Wrz_Paź_Lis_Gru'.split('_'), // OPTIONAL, short months Array, use first three letters if not provided
-          ordinal: n => `${n}`, // ordinal Function (number) => return number + output
-          relativeTime: { // relative time format strings, keep %s %d as the same
-            future: 'za %s', // e.g. in 2 hours, %s been replaced with 2hours
-            past: '%s temu',
-            s: 'kilka sekund',
-            m: 'minutę',
-            mm: '%d minut',
-            h: 'godzinę',
-            hh: '%d godzin', // e.g. 2 hours, %d been replaced with 2
-            d: 'dzień',
-            dd: '%d dni',
-            M: 'miesiąc',
-            MM: '%d miesięcy',
-            y: 'rok',
-            yy: '%d lat'
-          }
-       }*/
-};
-
-
-export default {
-  components: {
-    ganttElasticHeader: {template:`<span>your header</span>`}, // or Header
-    ganttElastic: GanttElastic,
-    ganttElasticFooter: {template:`<span>your footer</span>`},
-  },
-  data() {
-    return {
-      tasks: tasks,
-      options: options
+      },
     };
-  }  
-}
+  },
+  methods: {
+    getDate(hours) {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const timeStamp = new Date(
+        `${day}-${month}-${year} 00:00:00`
+      ).getTime();
+      return new Date(timeStamp + hours * 60 * 60 * 1000).getTime();
+    },
+  },
+};
 </script>
-
 <style>
 </style>
